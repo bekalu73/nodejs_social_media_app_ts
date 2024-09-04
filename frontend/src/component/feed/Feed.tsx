954c78577a204a594284bd2fef3df921f291db0d
bekalu73 <bekalusisay2010@gmail.com>
2024-09-04 17:02:15 +0300

    data descending order to posts

--- frontend/src/component/feed/Feed.tsx
+++ frontend/src/component/feed/Feed.tsx
@@ -1,27 +1,41 @@
+import React, { useEffect } from "react";
 import Post from "../post/Post";
 import Share from "../share/Share";
 import { useDispatch, useSelector } from "react-redux";
 import { AppDispatch } from "../../context/store";
 import { posts } from "../../context/Actions/post/postActions";
 import { RootState } from "../../context/Reducer/rootReducer";
-import { useEffect } from "react";
+import { Post as PostType } from "../../types/types";
 
 const Feed: React.FC = () => {
   const dispatch: AppDispatch = useDispatch();
 
-  const postsData = useSelector((state: RootState) => state.postData);
-  console.log(postsData);
+  // Ensure postsData is correctly typed and logged
+  const postsData = useSelector(
+    (state: RootState) => state.postData as PostType[]
+  );
+  console.log("Posts Data:", postsData);
 
   useEffect(() => {
     dispatch(posts());
   }, [dispatch]);
 
+  // Convert strings to Date objects if necessary and sort posts
+  const sortedPosts = postsData
+    .map((post) => ({
+      ...post,
+      date: new Date(post.date),
+    }))
+    .sort((a, b) => b.date.getTime() - a.date.getTime());
+
+  console.log("Sorted Posts:", sortedPosts);
+
   return (
     <div className="flex-1 p-5">
       <div className="space-y-4">
         <Share />
-        {postsData && postsData.length > 0 ? (
-          postsData.map((post) => <Post key={post.id} post={post} />)
+        {sortedPosts.length > 0 ? (
+          sortedPosts.map((post) => <Post key={post.id} post={post} />)
         ) : (
           <p>No posts available.</p>
         )}
